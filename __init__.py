f626d00e6e0eb3e57aa7aaa03d200003854a7641
Nate Singer <nathaniel@singer.cloud>
2020-04-05 09:21:55 -0500

    POST component, gitignore for API key, header builder

--- __init__.py
+++ __init__.py
@@ -2,27 +2,44 @@
 HTB API Wrapper for HnH
 Author: Nate Singer
 Depends:
-    requests (pip install requests)
+    import: requests (pip install requests)
+    import: json (should come out of the box with python3)
+    file: server.apikey (create this in the relative root and place server's API key on line 1)
 """
 import requests
+import json
 
-wrapper_version='v0.1'
+version='v0.1'
 
 class HnHBartender:
-    user_agent='HnH_API_WRAPPER_' + wrapper_version
-    base_url='https://www.hackthebox.eu/api'
+    def __init__(self):
+        with open('server.apikey','r') as f: self.api_key = f.read() #pull in API key
+        self.base_url = 'https://www.hackthebox.eu/api'
 
-    def __init__(self, pass_api_key=None, pass_user_agent=user_agent, pass_base_url=base_url):
-        self.api_key = pass_api_key
-        self.headers = {'User-Agent':pass_user_agent}
-        self.api_base_url = pass_base_url
+    def __build_headers(self, *argv):
+        header = {'User-Agent':'HnH_API_WRAPPER_' + version} #build user agent as base of header chain
+        for arg in argv:
+            header.update(arg)
+        return header
 
-    def __auth(self, pass_path):
-        return pass_path + '?api_token=' + self.api_key
+    def get_all_machines(self):
+        response=requests.get('{}/machines/get/all/?api_token={}'.format(self.base_url, self.api_key), headers=self.__build_headers())
+        print("Code: {}, Response: {}".format(response, response.json()))
 
-    def query_example(self):
-        response=requests.get(self.base_url + self.__auth('/machines/owns/'), headers=self.headers)
-        print(response.json())
+    def get_user_id(self, member=None):
+        u = "{}/user/id?api_token={}".format(self.base_url, self.api_key)
+        d = {'username':'{}'.format(member)}
+        h = self.__build_headers({'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'})
 
-request = HnHBartender()
-request.query_example()
+        response=requests.post(u, data=d, headers=h)
+        print("Code: {}, Response: {}".format(response, response.content))
+
+bartender = HnHBartender()
+bartender.get_all_machines()
+bartender.get_user_id('rudem')
+
+
+"""
+Queries for potential future use:
+    {base}/machines/owns/?api_token={apitoken} //this gives the owned machines of the user who's API key is passed
+"""
